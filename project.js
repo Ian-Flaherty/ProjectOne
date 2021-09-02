23fe10b365a8164ea871d5b280bcf539c811981a
Ian-Flaherty <Ianflaherty@yahoo.com>
2021-09-02 15:04:33 -0500

    just style and note left

--- project.js
+++ project.js
@@ -1,24 +1,22 @@
 
-// const foodDiv = document.querySelector(`#food-data`);
-// const input = document.querySelector("input");
-// const textInputBox = document.querySelector("#food");
-// const button = document.querySelector('#button');
+const foodDiv = document.querySelector(`#food-data`);
+const input = document.querySelector("input");
+const textInputBox = document.querySelector("#food");
+const button = document.querySelector('#button');
 
 async function fetchData(food) {
   let url = `https://api.edamam.com/api/nutrition-data?app_id=327e7fb3&app_key=29781e8ea7a4666fe38782b16f434a63&nutrition-type=cooking&ingr=${food}?fulltext=true`
   try {
     let response = await axios.get(url);
-    
-    processLabel(response);
+    processDietary(response);
     processCarbs(response);
     console.log(response.data);
   } catch (error) {
     console.log(error);
   }
 }
-function removeOldContent() {
-}
-function processLabel(response) {
+
+function processDietary(response) {
   
   let ul = document.getElementById('health-items');
   ul.innerHTML = "";
